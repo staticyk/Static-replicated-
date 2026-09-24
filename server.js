fb91c7bc2b05fb1c7ddc37e4eade4b303b5329b5
staticyk <kaden44o@icloud.com>
2026-09-23 22:23:02 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = createServer();
+const bare = createBareServer('/bare/');
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.on('request', (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on('upgrade', (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Static server running on port ${PORT}`);
+});
