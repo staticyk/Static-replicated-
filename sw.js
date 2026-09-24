b7d3c9ec7d48260a6a0868aea4b0d0cac19a1e94
staticyk <kaden44o@icloud.com>
2026-09-23 23:25:35 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
 importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.bundle.js');
 importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.0.0/dist/uv.config.js');
 
-if (typeof UVServiceWorker !== 'undefined') {
-  const uv = new UVServiceWorker();
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin + '/service/')) {
-      event.respondWith(uv.fetch(event));
-    }
-  });
-}
+const uv = typeof UVServiceWorker !== 'undefined' ? new UVServiceWorker() : null;
+
+self.addEventListener('fetch', (event) => {
+  if (uv && event.request.url.startsWith(self.location.origin + '/service/')) {
+    event.respondWith(uv.fetch(event));
+  }
+});
